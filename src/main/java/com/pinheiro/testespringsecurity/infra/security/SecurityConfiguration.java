1e9f748da5bfd9594863438173dfea331c561719
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-05-14 21:37:20 -0300

    Arrumando bug de instanciacao do encoder de senha

--- src/main/java/com/pinheiro/testespringsecurity/infra/security/SecurityConfiguration.java
+++ src/main/java/com/pinheiro/testespringsecurity/infra/security/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
